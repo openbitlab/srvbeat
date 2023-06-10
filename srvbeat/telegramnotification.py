7db1fd8c0cfb0b74fddb192417d21a3ea48ef967
AlessandroPaparella <61472307+AlessandroPaparella@users.noreply.github.com>
2023-06-10 21:44:28 +0200

    optional etcd feature

--- srvbeat/telegramnotification.py
+++ srvbeat/telegramnotification.py
@@ -8,8 +8,17 @@ class TelegramNotification:
 	def __init__(self, conf):
 		self.apiToken = conf['telegram']['apiToken'].strip('\"')
 		self.chatId = conf['telegram']['chatId'].strip('\"')
-		self.client_etcd = Client(conf['general']['etcdEndpoint'], 2379)
-		self.etcd_id = self.client_etcd.status().header.member_id
+		if conf['general']['etcdEndpoint'] != '':
+			self.client_etcd = Client(conf['general']['etcdEndpoint'], 2379)
+			self.etcd_id = self.client_etcd.status().header.member_id
+		else:
+			self.client_etcd = None
+			self.etcd_id = None
+
+	def checkSend(self):
+		if self.client_etcd is None:
+			return True
+		return self.etcd_id == self.client_etcd.status().leader
 
 	def sendPhoto(self, photo):
 		os.system('curl -F photo=@"./%s" https://api.telegram.org/bot%s/sendPhoto?chat_id=%s' % (photo, self.apiToken, self.chatId))
@@ -19,7 +28,7 @@ class TelegramNotification:
 		args = f"text={st}&chat_id={self.chatId}"
 		if not notify:
 			args += '&disable_notification=true'
-		if self.etcd_id == self.client_etcd.status().leader:
+		if self.checkSend():
 			requests.get(f'https://api.telegram.org/bot{self.apiToken}/sendMessage?{args}').json()
 
 	def format(self, name, string):
