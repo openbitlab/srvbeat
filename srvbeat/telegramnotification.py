e759654baadeb1dce96beb1839fcfa2bf8895139
Davide Gessa (dakk) <gessadavide@gmail.com>
2023-11-03 09:35:54 +0100

    surround send with try catch

--- srvbeat/telegramnotification.py
+++ srvbeat/telegramnotification.py
@@ -1,25 +1,38 @@
-import os
 import json
-import requests
+import os
 import urllib.parse
 
+import requests
+from requests.exceptions import ConnectionError
+
+
 class TelegramNotification:
-	def __init__(self, conf):
-		self.apiToken = conf['telegram']['apiToken'].strip('\"')
-		self.chatId = conf['telegram']['chatId'].strip('\"')
+    def __init__(self, conf):
+        self.apiToken = conf["telegram"]["apiToken"].strip('"')
+        self.chatId = conf["telegram"]["chatId"].strip('"')
 
-	def sendPhoto(self, photo):
-		os.system('curl -F photo=@"./%s" https://api.telegram.org/bot%s/sendPhoto?chat_id=%s' % (photo, self.apiToken, self.chatId))
+    def sendPhoto(self, photo):
+        os.system(
+            'curl -F photo=@"./%s" https://api.telegram.org/bot%s/sendPhoto?chat_id=%s'
+            % (photo, self.apiToken, self.chatId)
+        )
 
-	def send(self, st, notify = True):
-		print(st.encode('utf-8'))
-		args = f"text={st}&chat_id={self.chatId}"
-		if not notify:
-			args += '&disable_notification=true'
-		requests.get(f'https://api.telegram.org/bot{self.apiToken}/sendMessage?{args}').json()
+    def send(self, st, notify=True):
+        print(st.encode("utf-8"))
+        args = f"text={st}&chat_id={self.chatId}"
+        if not notify:
+            args += "&disable_notification=true"
+        try:
+            requests.get(
+                f"https://api.telegram.org/bot{self.apiToken}/sendMessage?{args}"
+            ).json()
+        except ConnectionError as e:
+            print("Conncetion error.")
 
-	def format(self, name, string):
-		return urllib.parse.quote('#' + name + ' ' + string)
+    def format(self, name, string):
+        return urllib.parse.quote("#" + name + " " + string)
 
-	def getUpdates(self):
-		return requests.get(f'https://api.telegram.org/bot{self.apiToken}/getUpdates').json()
+    def getUpdates(self):
+        return requests.get(
+            f"https://api.telegram.org/bot{self.apiToken}/getUpdates"
+        ).json()
